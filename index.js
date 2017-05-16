719f708682ad186f5e1179f74c2a7124a913fa4e
Laurence Ruberl <bknd225@gmail.com>
2017-05-16 13:36:09 -0400

    Add event listener and grab roster list

--- index.js
+++ index.js
@@ -6,5 +6,11 @@
         const form = ev.target
         const rosterList = document.querySelector('rosterList')
     }
-    
+
 })
+
+
+
+
+
+// for list buttons use .onclick to reference a function within this file
