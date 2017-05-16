5e06651ede4f176aed93b3db2a948b591bb3827f
Laurence Ruberl <bknd225@gmail.com>
2017-05-16 14:24:50 -0400

    Add names to roster on submit press

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 (function(){
     const nameForm = document.querySelector('form')
 
+
+    // const buildLiButtons = () => {
+    //     const delbutton = document.createElement('button')
+    //     delbutton.onclick = () => {
+
+    //     }
+    // }
+
+    const buildListItem = (name) => {
+        const li = document.createElement('li')
+        li.textContent = name
+
+        return li
+    }
+
     const handleSubmit = (ev) => {
-        debugger
         ev.preventDefault()
         const form = ev.target
         const rosterList = document.querySelector('.rosterList')
+        console.log(rosterList)
+        const name = form.name.value
+        rosterList.appendChild(buildListItem(name))
     }
-
-
+    
+    
     nameForm.addEventListener('submit', handleSubmit)
 })()
 
