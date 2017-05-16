d8e220a7467e355094f0a807acdf7ae9a3a496f6
Laurence Ruberl <bknd225@gmail.com>
2017-05-16 12:00:50 -0400

    Begin set up of js function; Set up HTML document with a form

---
+++ index.js
@@ -0,0 +1,10 @@
+(function(){
+    const nameForm = document.querySelector('form')
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault()
+        const form = ev.target
+        const rosterList = document.querySelector('rosterList')
+    }
+    
+})
