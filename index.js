f7f73aea6e18065f11900ef1b2af72ce7d4bf1db
Laurence Ruberl <bknd225@gmail.com>
2017-05-16 16:08:49 -0400

    Add promote button

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 (function(){
     const nameForm = document.querySelector('form')
 
-
-    const buildLiButtons = () => {
-        const buttonField = document.createElement('div')
+    const buildDelButton = () => {
         const delbutton = document.createElement('button')
-        buttonField.style.display = 'inline-block'
-        buttonField.style.backgroundColor = '#ff3300'
+        delbutton.style.backgroundColor = '#ff3300'
         delbutton.textContent = 'Delete'
         delbutton.addEventListener('click', (ev) => {
             let button = ev.target
             console.log(button.parentNode.parentNode)
             button.parentNode.parentNode.remove()
         })
-        buttonField.appendChild(delbutton)
+        return delbutton
+    }
+
+    const buildPromoteButton = () => {
+        const promoteButton = document.createElement('button')
+        promoteButton.style.backgroundColor = '#0000ff'
+        promoteButton.textContent = 'Promote'
+        promoteButton.addEventListener('click', (ev) => {
+            let button = ev.target
+            button.parentNode.parentNode.style.backgroundColor = '#00ff00'
+        })
+        return promoteButton
+    }
+
+    const buildLiButtons = () => {
+        const buttonField = document.createElement('div')
+        // const delbutton = document.createElement('button')
+        buttonField.style.display = 'inline-block'
+        // delbutton.style.backgroundColor = '#ff3300'
+        // delbutton.textContent = 'Delete'
+        // delbutton.addEventListener('click', (ev) => {
+        //     let button = ev.target
+        //     console.log(button.parentNode.parentNode)
+        //     button.parentNode.parentNode.remove()
+        // })
+        buttonField.appendChild(buildDelButton())
+        buttonField.appendChild(buildPromoteButton())
         return buttonField
         }
 
